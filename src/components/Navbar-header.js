9ea69ca5fa07625eb434d6b6a8282117f7f2da96
nguyenphuonglinh2k <nguyenphuonglinh11102000@gmail.com>
2020-12-19 15:36:00 +0700

    update profile

--- src/components/Navbar-header.js
+++ src/components/Navbar-header.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 export default function NavbarHeader(props) {
-    const { userImageUrl } = JSON.parse(localStorage.getItem('user'));
+    const user = JSON.parse(localStorage.getItem('user'));
     const { setIsProfileOpen, sidebarOpen, setSidebarOpen } = props;
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const toggle = () => setDropdownOpen(prevState => !prevState);
@@ -26,21 +26,23 @@ export default function NavbarHeader(props) {
                 <div onClick={() => setSidebarOpen(!sidebarOpen)} className="menu-btn">
                     <img src="https://cdn.glitch.com/af45ea57-cc17-431c-a29e-191393077cfe%2Flist.svg?v=1596545874905" alt="" />
                 </div>
-                <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-                    <DropdownToggle>
-                        <div className="header-profile">
-                            <img 
-                                className="rounded-circle header-profile-user" 
-                                src={userImageUrl} 
-                                alt="Header Avatar" 
-                            />
-                        </div>
-                    </DropdownToggle>
-                    <DropdownMenu right className="mt-2">
-                        <DropdownItem onClick={onClickShowProfile}>Profile</DropdownItem>
-                        <DropdownItem onClick={onClickLogOut}>Log out</DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
+                { user && 
+                    <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                        <DropdownToggle>
+                            <div className="header-profile">
+                                <img 
+                                    className="rounded-circle header-profile-user" 
+                                    src={user.userImageUrl} 
+                                    alt="Header Avatar" 
+                                />
+                            </div>
+                        </DropdownToggle>
+                        <DropdownMenu right className="mt-2">
+                            <DropdownItem onClick={onClickShowProfile}>Profile</DropdownItem>
+                            <DropdownItem onClick={onClickLogOut}>Log out</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                }
             </div>
         </div>
     );
